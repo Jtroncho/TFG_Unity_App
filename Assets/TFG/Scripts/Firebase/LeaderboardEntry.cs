89a16fa43af124bd9c10505eb16fb31e51cda516
Jtroncho <joaneltroncho@gmail.com>
2020-06-11 22:29:21 +0200

    LeaderBoard Entries.

---
+++ Assets/TFG/Scripts/Firebase/LeaderboardEntry.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace TFG.Database
+{
+    public class LeaderboardEntry
+    {
+        public string uid = "";
+        public string email = "";
+        public long score = 0;
+
+        public LeaderboardEntry()
+        {
+        }
+
+        public LeaderboardEntry(string uid, string email, long score)
+        {
+            this.uid = uid;
+            this.email = email;
+            this.score = score;
+        }
+
+        public Dictionary<string, object> ToDictionary()
+        {
+            Dictionary<string, object> result = new Dictionary<string, object>();
+            result["email"] = email;
+            result["score"] = score;
+
+            return result;
+        }
+    }
+}
+
